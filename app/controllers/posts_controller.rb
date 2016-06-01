909e35062633e6af756cacadb1e67109ca42d4f2
Alisher778 <internet-docs@mail.ru>
2016-06-01 12:04:58 -0400

    Copying from my directory

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,59 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @posts = Post.all.order("created_at DESC").paginate(page: params[:page], per_page: 3)
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Your post was successfully saved"
+      redirect_to posts_path(@post)
+    else
+      flash.now[:error] = "Something went wrong"
+      render :new
+    end
+  end
+
+  def update
+
+    if @post.update(post_params)
+      flash[:notice] = "Your post successfully updated"
+      redirect_to posts_path(@post)
+    else
+      flash[:error] = "Something is wrong"
+      render :edit
+  end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, error: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+   
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 3, maximum: 75 }, uniqueness: true
+  validates :text, presence: true, length: { minimum: 15, maximum: 350 }, uniqueness: true
+end

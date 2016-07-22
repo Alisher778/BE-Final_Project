92674004214bb802e46f63c4a516f08dd33c46eb
Alisher778 <internet-docs@mail.ru>
2016-07-22 16:50:52 -0400

    added Uer_id to Posts after issue on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707031041) do
+ActiveRecord::Schema.define(version: 20160722203450) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
